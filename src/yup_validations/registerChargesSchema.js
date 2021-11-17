dec6c2513297848a6128551018949f36a24bc783
Jade Cordeiro <jade.dev@hotmail.com>
2021-11-16 21:49:38 -0300

    correção da pagina de cobranças

--- src/yup_validations/registerChargesSchema.js
+++ src/yup_validations/registerChargesSchema.js
@@ -1,11 +1,11 @@
 const yup = require('./config');
 
 const registerChargeSchema = yup.object().shape({
-  clienteId: yup.string().required(),
-  descricao: yup.string().required(),
+  clientId: yup.string().required(),
+  description: yup.string().required(),
   status: yup.string().required(),
-  valor: yup.string().required(),
-  vencimento: yup.string().required()
+  value: yup.string().required(),
+  due_date: yup.string().required()
 });
 
 module.exports = registerChargeSchema;
